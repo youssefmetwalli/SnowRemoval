9fdcea1a67be2e77e39fccae440f0a61374cf5e0
AyanaAkutsu <s1290002.u.aizu@gmail.com>
2025-10-17 20:34:38 +0900

    ユーザー名表示の不具合修正・日付の表示

--- src/components/UserName.tsx
+++ src/components/UserName.tsx
@@ -1,9 +1,23 @@
 
 export const UserName = (): JSX.Element => {
-  const userName = localStorage.getItem('loggedInUser');
-  
+  const rawUserData = localStorage.getItem('loggedInUser');
+  let name: string | null = null;
+  try {
+    if(rawUserData){
+      const userData = JSON.parse(rawUserData);
+      name = userData?.field_1754549790;
+    }
+    
+  } catch (error) {
+    console.error("Failed to parse JSON from localStrage.", error);
+  }
+  const date = new Date();
+  const weekday = new Intl.DateTimeFormat('jp-JP', {
+    weekday: "short"
+  }).format(date);
+
   return (
-    <div className="w-full text-white shadow-md rounded-b-2xl">
+    <div className="w-full text-white shadow-md rounded-b-2xl mb-3">
       <div className="flex items-center justify-between bg-white text-gray-800 rounded-xl shadow p-4">
         <div className="flex items-center gap-4">
           {/* <Avatar className="w-12 h-12 rounded-full shadow-md">
@@ -12,11 +26,11 @@ export const UserName = (): JSX.Element => {
               </AvatarFallback>
             </Avatar> */}
           <div className="flex flex-row items-center">
-            <p className="font-semibold text-base pr-5">{userName}</p>
+            <p className="font-semibold text-base pr-5">{name}</p>
             {/* <p className="text-sm text-gray-500">作業責任者</p> */}
           </div>
         </div>
-        <p className="text-sm text-gray-400">2025年2月2日(日)</p>
+        <p className="text-sm text-gray-400">{date.getFullYear()}年{date.getMonth()+1}月{date.getDate()}日({weekday})</p>
       </div>
     </div>
   );
