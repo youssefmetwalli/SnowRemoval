d350a17ac96938c670511cdfd525ed54244343ea
AyanaAkutsu <s1290002.u.aizu@gmail.com>
2025-10-23 20:41:13 +0900

    クエリパラメータの設定

--- src/hook/getReport.ts
+++ src/hook/getReport.ts
@@ -6,13 +6,16 @@ export const useReport = () => {
   const [data, setData] = useState<ReportGetData[]>();
   const [isLoading, setLoading] = useState(true);
   const [isError, setError] = useState(false);
+  const loginUser = localStorage.getItem("loggedInUser");
+  const parsedLoginUser = JSON.parse(loginUser!);
+  const loginUserId = parsedLoginUser.field_1754635302[1];
 
   useEffect(() => {
     (async () => {
       try {
         console.log("get送信");
         const res = await apiClient.get<ReportGetData[]>(
-          "table_1754551086/records/"
+          `table_1754551086/records/?limit=100&_field_workerId=${loginUserId}&filter_1761218755&field_workDate=-7`
         );
         setData(res);
       } catch (err) {
