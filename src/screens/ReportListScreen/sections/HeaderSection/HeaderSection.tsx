3687a0c82cbfcc37419a29beff1ef5114044078a
buumeran <bmrn888555@gmail.com>
2025-10-24 12:26:35 +0900

    見た目の修正とファイルの場所の移動

--- src/screens/ReportListScreen/sections/HeaderSection/HeaderSection.tsx
+++ src/screens/ReportListScreen/sections/HeaderSection/HeaderSection.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../../../../components/ui/button";
 import { Input } from "../../../../components/ui/input";
 import { useState } from "react";
-import { FilterMenu } from "../../FilterMenu";
+import { FilterMenu } from "../FilterMenu/FilterMenu";
 
 
 interface HeaderSectionProps {
